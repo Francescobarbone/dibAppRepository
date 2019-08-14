35adc09b4e7ce788299f83c3c028836cce9b73d1
gabrieleloprieno <gabrieleloprieno11@gmail.com>
2019-08-14 13:33:30 +0200

    MainActivity updated, login

--- app/src/main/java/com/dibapp/dibapp/MainActivity.java
+++ app/src/main/java/com/dibapp/dibapp/MainActivity.java
@@ -1,18 +1,98 @@
 package com.dibapp.dibapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailId, password;
+    Button btnSignIn;
+    TextView tvSignUp;
     FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSignIn = findViewById(R.id.button);
+        tvSignUp = findViewById(R.id.textView);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser fireBaseUser = firebaseAuth.getCurrentUser();
+                if (fireBaseUser != null) {
+                    Toast.makeText(MainActivity.this, "Login avvenuto con successo", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(MainActivity.this, "Per favore, effettuare il login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = emailId.getText().toString();
+                String psw = password.getText().toString();
+                if(email.isEmpty()){
+                    emailId.setError("Per favore, inserisca l'indirizzo e-mail");
+                    emailId.requestFocus();
+                } else if(psw.isEmpty()){
+                    password.setError("Per favore, inserisca la password");
+                    password.requestFocus();
+                } else if(email.isEmpty() && psw.isEmpty())
+                    Toast.makeText(MainActivity.this, "I campi sono vuoti!", Toast.LENGTH_SHORT).show();
+                else if (!(email.isEmpty() && psw.isEmpty())){
+                    firebaseAuth.signInWithEmailAndPassword(email, psw).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Errore durante il login, riprova", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Intent intToHome = new Intent(MainActivity.this, HomeActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                } else
+                    Toast.makeText(MainActivity.this, "Errore durante il login", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intSignUp = new Intent (MainActivity.this, RegistrazioneActivity.class);
+                startActivity(intSignUp);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
+
