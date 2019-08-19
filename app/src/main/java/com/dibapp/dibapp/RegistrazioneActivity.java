33cc4eac76576fefa24fb4eef29a83def774f997
gabrieleloprieno <gabrieleloprieno11@gmail.com>
2019-08-19 10:50:53 +0200

    Denied access from Login to Home once logged out

--- app/src/main/java/com/dibapp/dibapp/RegistrazioneActivity.java
+++ app/src/main/java/com/dibapp/dibapp/RegistrazioneActivity.java
@@ -50,41 +50,33 @@ public class RegistrazioneActivity extends AppCompatActivity {
                 else if(!(email.isEmpty() && pwd.isEmpty())){
                     //Aggiungi procedura di verifica e-mail
                     if(email.endsWith("@studenti.uniba.it") || email.endsWith("@uniba.it")) {
-
-                        firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(RegistrazioneActivity.this, new OnCompleteListener<AuthResult>() {
+                        firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
                             @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(!task.isSuccessful()) {
-                                    Toast.makeText(RegistrazioneActivity.this,"Registrazione non Riuscita, Riprova!",Toast.LENGTH_SHORT).show();
-                                }
-                                else{
+                            public void onComplete(Task<AuthResult> task){
+                                if(task.isSuccessful()) {
                                     firebaseAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                                         @Override
                                         public void onComplete(@NonNull Task<Void> task) {
-                                            if(task.isSuccessful()){
-                                                Toast.makeText(RegistrazioneActivity.this,"Registrazione avvenuta con successo, Controlla la tua e-mail",
-                                                        Toast.LENGTH_SHORT).show();
-
-                                            }
-                                            else{
-                                                Toast.makeText(RegistrazioneActivity.this,task.getException().getMessage(),
-                                                        Toast.LENGTH_SHORT).show();
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(RegistrazioneActivity.this, "Registrazione avvenuta con successo. Controlla la casella di posta per la verifica.", Toast.LENGTH_LONG).show();
+                                                emailId.setText("");
+                                                password.setText("");
+                                                finishAffinity();
+                                            } else {
+                                                Toast.makeText(RegistrazioneActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                                emailId.setText("");
+                                                password.setText("");
                                             }
                                         }
                                     });
-                                    startActivity(new Intent(RegistrazioneActivity.this, MainActivity.class));
                                 }
                             }
                         });
+                } else
+                     Toast.makeText(RegistrazioneActivity.this, "Puoi eseguire la registrazione  solo con un e-mail istituzionale",Toast.LENGTH_SHORT).show();
 
-                        }
-                    else {
-                        Toast.makeText(RegistrazioneActivity.this, "Puoi eseguire la registrazione  solo con un e-mail istituzionale",Toast.LENGTH_SHORT).show();
-                    }
-
-                }else{
-                    Toast.makeText(RegistrazioneActivity.this, "Errore!", Toast.LENGTH_SHORT).show();
-                }
+            } else
+                Toast.makeText(RegistrazioneActivity.this, "I campi sono vuoti!",Toast.LENGTH_SHORT).show();
             }
         });
     }
